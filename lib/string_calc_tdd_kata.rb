60db2007ad0d48a347d3b8ccf0e93e14e78c44b0
VeereshKumar14 <rveereshkumarr@gmail.com>
2025-08-02 00:44:51 +0530

    Add logic for new line delimiter

--- lib/string_calc_tdd_kata.rb
+++ lib/string_calc_tdd_kata.rb
@@ -1,8 +1,9 @@
 class StringCalcTddKata
     def self.add(input)
         return 0 if input.strip.empty?
+        delimiter = /,|\n/
 
-        sum_of_numbers = input.split(',').map(&:to_i)            
+        sum_of_numbers = input.split(delimiter).map(&:to_i)            
 
         sum_of_numbers.sum
     end
