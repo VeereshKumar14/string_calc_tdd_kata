5a81a9dc48c0837e56a00464c97f809c672ecdaf
VeereshKumar14 <rveereshkumarr@gmail.com>
2025-08-02 00:37:22 +0530

    Add logic for single digit and comma-separated numbers

--- lib/string_calc_tdd_kata.rb
+++ lib/string_calc_tdd_kata.rb
@@ -1,5 +1,9 @@
 class StringCalcTddKata
     def self.add(input)
         return 0 if input.strip.empty?
+
+        sum_of_numbers = input.split(',').map(&:to_i)            
+
+        sum_of_numbers.sum
     end
 end
