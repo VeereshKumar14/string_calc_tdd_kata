15f3e208c64c6c3f8d20399b1c6ccc8f1203abbd
VeereshKumar14 <rveereshkumarr@gmail.com>
2025-08-02 01:47:11 +0530

    refactor existing code with readability and readability

--- lib/string_calc_tdd_kata.rb
+++ lib/string_calc_tdd_kata.rb
@@ -1,34 +1,50 @@
 class StringCalcTddKata
-    def self.add(input)
-        return 0 if input.strip.empty?
-        
-        delimiter = /,|\n/
-        numbers_str = input
+	DEFAULT_DELIMITERS = [",", "\n"].freeze
+	MAX_NUMBER = 1000
+	MIN_NUMBER = 0
 
-        if input.start_with?("//")
-            delimiter_section, numbers_str = input.split("\n", 2)
-            
-            # Match multiple delimiters like [*], [%%], etc.
-            custom_delimiters = delimiter_section.scan(/\[([^\]]+)\]/).flatten
+	def self.add(input)
+		return 0 if input.strip.empty?
 
-            if custom_delimiters.any?
-                escaped_delimiters = custom_delimiters.map { |d| Regexp.escape(d) }
-                delimiter = Regexp.new(escaped_delimiters.join("|"))
-            else
-                # single delimiter like //;\n
-                delimiter = Regexp.new(Regexp.escape(delimiter_section[2]))
-            end    
-        end
+		numbers_str = input
 
-        sum_of_numbers = numbers_str.split(delimiter).map(&:to_i)  
+		delimiters, numbers_string = extract_delimiters_and_numbers(input)
+    sum_of_numbers = split_numbers(numbers_str, delimiters)
 
-        # Negative check
-        negatives = sum_of_numbers.select { |n| n < 0 }
-        unless negatives.empty?
-            raise ArgumentError, "negatives not allowed: #{negatives.join(', ')}"
-        end
+		# Negative check
+		raise_for_negatives(sum_of_numbers)
 
-        # Ignore numbers > 1000
-        sum_of_numbers.reject { |num| num > 1000 }.sum
-    end
+		# Ignore numbers > 1000
+		sum_of_numbers.reject { |num| num > MAX_NUMBER }.sum
+	end
+
+	private
+
+	 def self.extract_delimiters_and_numbers(input)
+    return [DEFAULT_DELIMITERS, input] unless input.start_with?("//")
+
+    delimiter_line, number_part = input.split("\n", 2)
+    delimiter_string = delimiter_line[2..]
+
+    delimiters = if delimiter_string.start_with?("[")
+								   delimiter_string.scan(/\[([^\]]+)\]/).flatten
+								 else
+										[delimiter_string]
+								 end
+
+    [DEFAULT_DELIMITERS + delimiters, number_part]
+  end
+
+  def self.split_numbers(numbers_string, delimiters)
+    escaped_delimiters = delimiters.map { |delimiter| Regexp.escape(delimiter) }
+    delimiter_regex = Regexp.new(escaped_delimiters.join("|"))
+    numbers_string.split(delimiter_regex).map(&:to_i)
+  end
+
+	def self.raise_for_negatives(numbers)
+    negatives = numbers.select { |num| num < MIN_NUMBER }
+		unless negatives.empty?
+			raise ArgumentError, "negatives not allowed: #{negatives.join(', ')}"
+		end
+  end
 end
